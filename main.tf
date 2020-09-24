177bce55f8ccbfdc7198ecbcfb559f9a0c74d99f
CS <62691147+panrajucs@users.noreply.github.com>
2020-09-24 23:05:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,21 +38,19 @@ resource "aws_lb_target_group" "tg" {
  
 }
 
-//Create NLB
 
-resource "aws_lb" "nlb" {
-  name               = "aws-nlb"
-  internal           = false
-  load_balancer_type = "network"
-  subnets            = ["subnet-034250c45e4a08fbf","subnet-0d47b4ff30b766a68","subnet-0e603ab3ebf20d410"]
-
-} 
-
-
-//Create Elastic IP address for NAT GW
-
-resource "aws_eip" "lb" {
-  vpc      = true
+resource "aws_instance" "server-1" {
+    ami = "ami-00514a528eadbc95b"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "AWS_KeyPair"
+    subnet_id = "subnet-034250c45e4a08fbf"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script1.sh")}"
+    tags = {
+        Name = "Home-Server-1"
+    }
 }
 
 
